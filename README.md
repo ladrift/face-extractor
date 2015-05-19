faa5c58b1c19aa0f85a9feb6892ecfa2e017d8dc
ladrift <cxfyzy@gmail.com>
2015-05-20 03:40:08 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# face_extract
+## Face Extractor
+
+# Usage:
+1. Dowonload [face++ python sdk](https://github.com/FacePlusPlus/facepp-python-sdk)
+2. Copy files into `facepp-python-sdk` directory
+3. Run `extract_face.py` using:`python extract_face.py path_to_img`
+4. Output image is saved as `img_name_face.png`
---
+++ print_assist.py
@@ -0,0 +1,13 @@
+from pprint import pformat
+def print_result(hint, result):
+    def encode(obj):
+        if type(obj) is unicode:
+            return obj.encode('utf-8')
+        if type(obj) is dict:
+            return {encode(k): encode(v) for (k, v) in obj.iteritems()}
+        if type(obj) is list:
+            return [encode(i) for i in obj]
+        return obj
+    print hint
+    result = encode(result)
+    print '\n'.join(['  ' + i for i in pformat(result, width = 75).split('\n')])
